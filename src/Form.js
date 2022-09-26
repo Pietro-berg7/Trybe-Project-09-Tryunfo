70abd1a3418f4ffcdf7957b253160edd2ed7e6ee
Pietro Bergamaschi <pietrovb25@gmail.com>
2022-09-26 18:58:55 -0300

    Requisito 1 - ESLint fix

--- src/Form.js
+++ src/Form.js
@@ -4,45 +4,88 @@ class Form extends React.Component {
   render() {
     return (
       <form>
-        <fieldset>
-          <p>
-            <label htmlFor="">Nome da Carta:</label>
-            <input data-testid="name-input" type="text" />
-          </p>
-          <p>
-            <label htmlFor="">Descrição da carta:</label>
-            <textarea data-testid="description-input" name="descricao" id="" cols="30" rows="10" />
-          </p>
-          <p>
-            <label htmlFor="">Atributo 1:</label>
-            <input data-testid="attr1-input" type="number" name="" id="" />
-          </p>
-          <p>
-            <label htmlFor="">Atributo 2:</label>
-            <input data-testid="attr2-input" type="number" name="" id="" />
-          </p>
-          <p>
-            <label htmlFor="">Atributo 3:</label>
-            <input data-testid="attr3-input" type="number" name="" id="" />
-          </p>
-          <p>
-            <label htmlFor="">Imagem:</label>
-            <input data-testid="image-input" type="text" />
-          </p>
-          <p>
-            <label htmlFor="">Raridade:</label>
-            <select data-testid="rare-input" name="" id="">
+        <p>
+          <label htmlFor="nomeDaCarta">
+            Nome da Carta:
+            <input data-testid="name-input" type="text" id="nomeDaCarta" />
+          </label>
+        </p>
+        <p>
+          <label htmlFor="descricaoDaCarta">
+            Descrição da carta:
+            <textarea
+              data-testid="description-input"
+              name="descricao"
+              id="descricaoDaCarta"
+              cols="30"
+              rows="10"
+            />
+          </label>
+        </p>
+        <p>
+          <label htmlFor="attr1">
+            Atributo 1:
+            <input
+              data-testid="attr1-input"
+              type="number"
+              name=""
+              id="attr1"
+            />
+          </label>
+        </p>
+        <p>
+          <label htmlFor="attr2">
+            Atributo 2:
+            <input
+              data-testid="attr2-input"
+              type="number"
+              name=""
+              id="attr2"
+            />
+          </label>
+        </p>
+        <p>
+          <label htmlFor="attr3">
+            Atributo 3:
+            <input
+              data-testid="attr3-input"
+              type="number"
+              name=""
+              id="attr3"
+            />
+          </label>
+        </p>
+        <p>
+          <label htmlFor="imagem">
+            Imagem:
+            <input
+              data-testid="image-input"
+              type="text"
+              id="imagem"
+            />
+          </label>
+        </p>
+        <p>
+          <label htmlFor="raridade">
+            Raridade:
+            <select data-testid="rare-input" name="" id="raridade">
               <option value="normal">Normal</option>
               <option value="raro">Raro</option>
               <option value="muito raro">Muito Raro</option>
             </select>
-          </p>
-          <p>
-            <label htmlFor="">Super Trunfo:</label>
-            <input data-testid="trunfo-input" type="checkbox" />
-          </p>
-          <button data-testid="save-button">Salvar</button>
-        </fieldset>
+          </label>
+        </p>
+        <p>
+          <label htmlFor="superTrunfo">
+            Super Trunfo:
+            <input
+              data-testid="trunfo-input"
+              type="checkbox"
+              id="superTrunfo"
+            />
+          </label>
+        </p>
+        <button type="submit" data-testid="save-button">Salvar</button>
       </form>
     );
   }

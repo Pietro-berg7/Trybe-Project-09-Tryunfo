5bafc998770f5fea8928b42d598808434c811fb0
Pietro Bergamaschi <pietrovb25@gmail.com>
2022-10-05 14:44:19 -0300

    Requisito 5 concluído

--- src/App.js
+++ src/App.js
@@ -20,10 +20,34 @@ class App extends React.Component {
     const value = type === 'checkbox' ? checked : event.target.value;
     this.setState({
       [name]: value,
-    });
+    }, () => this.validadeForm());
   };
 
   onSaveButtonClick = () => {};
+
+  validadeForm = () => {
+    const maxStat = 90;
+    const allStatMax = 210;
+    const {
+      cardName,
+      cardImage,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+    } = this.state;
+    if (
+      cardName !== ''
+      && cardImage !== ''
+      && cardDescription !== ''
+      && (Number(cardAttr1) >= 0 && Number(cardAttr1) <= maxStat)
+      && (Number(cardAttr2) >= 0 && Number(cardAttr2) <= maxStat)
+      && (Number(cardAttr3) >= 0 && Number(cardAttr3) <= maxStat)
+      && (Number(cardAttr1) + Number(cardAttr2) + Number(cardAttr3) <= allStatMax)
+    ) {
+      this.setState({ isSaveButtonDisabled: false });
+    } else this.setState({ isSaveButtonDisabled: true });
+  };
 
   render() {
     return (
